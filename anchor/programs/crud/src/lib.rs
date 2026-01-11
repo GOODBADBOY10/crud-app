fa248a6095533d5c553927ed0b819b6940f616df
Goodbadboy <selfmanademola@gmail.com>
2026-01-11 06:35:02 +0100

    refactor: update journal entry structure and rename initialization function

--- anchor/programs/crud/src/lib.rs
+++ anchor/programs/crud/src/lib.rs
@@ -1,3 +1,6 @@
+#![allow(clippy::result_large_err)]
+#![allow(unexpected_cfgs)]
+
 use anchor_lang::prelude::*;
 // use anchor_lang::system_program::{transfer, Transfer};
 
@@ -10,23 +13,25 @@ declare_id!("2rMvvFmGX97VYJWFTpnVgrGGptBvCcXBrGEd3BjtALXf");
 pub mod crud {
     use super::*;
 
-    pub fn initialize_crud(ctx: Context<InitializeCrud>) -> Result<()> {
+    pub fn create_journal_entry(
+        ctx: Context<CreateEntry>,
+    ) -> Result<()>{
         Ok(())
     }
 }
 
+
 #[derive(Accounts)]
-pub struct InitializeCrud<'info> {
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
+pub struct CreateEntry<'info> {}
+
+
 
 #[account]
 #[derive(InitSpace)]
-pub struct Crud {
+pub struct JournalEntrySpace {
+    pub owner: Pubkey,
     #[max_len(20)]
     pub title: String,
     #[max_len(50)]
-    pub description: String,
+    pub message: String,
 }
